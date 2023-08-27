b12653cb0194ec532bc84845052d2e3006c5956f
Nit <66027553+Nitlix@users.noreply.github.com>
2023-08-28 02:07:55 +0300

    [+] Server URL Provider via middleware

---
+++ src/app/middleware.js
@@ -0,0 +1,15 @@
+//==================================================
+// Allow server components to access the URL
+//==================================================
+export function middleware(request) {
+    // Store current request url in a custom header, which you can read later
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('x-url', request.url);
+
+    return NextResponse.next({
+        request: {
+            // Apply new request headers
+            headers: requestHeaders,
+        }
+    });
+}

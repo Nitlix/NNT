dc4a87adf3c604e58270f94e9451f3adbaa6aeae
Nit <66027553+Nitlix@users.noreply.github.com>
2023-10-03 19:55:23 +0100

    [+] Add Prisma and update imports

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+//==================================================================
+//
+// Prisma Client
+// Run "npx prisma db push" to update the database records.
+// Run "npx prisma generate" to update the Prisma Client types.
+// Run "npx prisma studio" to open the Prisma Studio.
+//
+//==================================================================
+
+import { PrismaClient } from '@prisma/client'
+export default new PrismaClient();

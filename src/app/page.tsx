fd8be25375c08f2e9ad012457e10bcd56c5d0483
Nit <66027553+Nitlix@users.noreply.github.com>
2023-11-03 18:41:55 +0100

    [/] FlameCSS, TW Additions, Custom font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import strretchSans from "@/fonts/STRRETCH_SANS/strretchSans";
 import { Translations } from "@/i18n/translations";
 import { getLocale } from "nitlix-i18n"
 
 const pageTranslations: Translations = {
     en: {
-        welcome: "Welcome!"
+        welcome: "WELCOME"
     }
 }
 
@@ -11,7 +12,7 @@ export default function(){
     const lang: any = getLocale();
 
     return <>
-        <h3 style={{width: "100%", height: "100vh"}} className="flex center">
+        <h3 className={`w-full h-[100vh] flex center ${strretchSans.className}`}>
             {
                 pageTranslations[lang].welcome
             }
---
+++ src/fonts/STRRETCH_SANS/strretchSans.tsx
@@ -0,0 +1,8 @@
+import localFont from 'next/font/local'
+
+const font =  localFont({
+    src: './font.woff2',
+    display: 'swap',
+})
+
+export default font

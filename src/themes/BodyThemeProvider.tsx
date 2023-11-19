96dd2d5618076d0f5ef9bf0422c004d7eb263aef
Nit <66027553+Nitlix@users.noreply.github.com>
2023-11-19 17:11:46 +0000

    [/] Cleanup imports

--- src/themes/BodyThemeProvider.tsx
+++ src/themes/BodyThemeProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, createContext, useRef } from "react";
+import { useEffect, useState, createContext } from "react";
 
 // ================================
 // Theme Context

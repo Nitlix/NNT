adf3ff7aa70b43342eb03199e1e57febcfc8bbf5
Nit <66027553+Nitlix@users.noreply.github.com>
2023-11-22 20:03:22 +0000

    [+] Neutral Grotesk

---
+++ src/fonts/NeutralGrotesk/NeutralGrotesk.tsx
@@ -0,0 +1,75 @@
+
+
+import localFont from 'next/font/local';
+
+// =====================
+// Neutral Grotesk
+// Font class Export.
+// =====================
+const font = localFont({
+    src: [
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '400'
+        },
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '500'
+        },
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '600'
+        },
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '700'
+        },
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '800'
+        },
+        {
+            path: './NaturalGrotesk-Regular.woff2',
+            style: 'normal',
+            weight: '900'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '400'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '500'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '600'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '700'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '800'
+        },
+        {
+            path: './NaturalGrotesk-RegularItalic.woff2',
+            style: 'italic',
+            weight: '900'
+        }
+    ],  
+    display: 'swap',
+});
+
+export default font;

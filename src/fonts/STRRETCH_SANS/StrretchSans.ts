fdae3a7b7e019b877ee31e2164c13ad9d09cbbfa
Nit <66027553+Nitlix@users.noreply.github.com>
2024-01-23 19:56:34 +0000

    [/] How it's supposed to be

---
+++ src/fonts/STRRETCH_SANS/StrretchSans.ts
@@ -0,0 +1,12 @@
+import localFont from 'next/font/local';
+
+// =====================
+// STRRETCH SANS 
+// Font class Export.
+// =====================
+const StrretchSans =  localFont({
+    src: './font.woff2',
+    display: 'swap',
+});
+
+export default StrretchSans;

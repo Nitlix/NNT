ff0b83b7817ac19071453b8a2ce3011e9aa176f0
Nitlix <n@nitlix.pro>
2024-11-08 20:58:10 +0000

    [/] Fixed Prisma development issues
    Less new connections will be made when using Prisma during development.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,5 +7,18 @@
 //
 //==================================================================
 
-import { PrismaClient } from '@prisma/client'
-export default new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
